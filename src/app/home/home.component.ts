c153a5c2238e70da94c0c0067477532135bd63c2
oliverwreath <skywalkerhunter@gmail.com>
2019-06-17 16:49:27 -0400

    Angular 7 Event binding, class& style binding.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  h1Style: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  firstClick() {
+    this.h1Style = true;
+  }
 }
