526b6e0dadd48806f97b374600b3e7f50dacaeaf
oliverwreath <skywalkerhunter@gmail.com>
2019-06-17 16:38:33 -0400

    declare NavComponent, import RouterTestingModule.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent
       ],
+      imports: [
+        RouterTestingModule,
+      ]
     }).compileComponents();
   }));
 
