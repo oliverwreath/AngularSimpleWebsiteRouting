4fdee855054b929fae48e58a7746a0fffd1e27b3
oliverwreath <skywalkerhunter@gmail.com>
2019-06-17 17:45:24 -0400

    imports: [         HttpClientModule       ]
          for testing.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
@@ -8,9 +9,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+      imports: [
+        HttpClientModule
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
