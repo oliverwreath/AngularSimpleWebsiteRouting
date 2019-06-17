6c221f9f3ae3cb3c8a4a7ff4d15284725a26da98
oliverwreath <skywalkerhunter@gmail.com>
2019-06-17 16:52:23 -0400

    create methods that are accessible anywhere.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   h1Style: boolean = false;
 
-  constructor() { }
+  constructor(private data: DataService) {
+  }
 
   ngOnInit() {
   }
 
   firstClick() {
-    this.h1Style = true;
+    this.data.firstClick();
   }
 }
